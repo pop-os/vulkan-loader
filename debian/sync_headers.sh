a6094623a7fba450bbd7a3c5af91c9f38fa0ee54
Timo Aaltonen <tjaalton@debian.org>
2023-06-06 16:47:13 +0300

    sync-headers: Support non-sdk tags.

--- debian/sync_headers.sh
+++ debian/sync_headers.sh
@@ -5,14 +5,14 @@ REPO=https://github.com/KhronosGroup/Vulkan-Headers.git
 if [ -n "$1" ]; then
   VERSION=$1
 else
-  VERSION=`dpkg-parsechangelog -S Version|sed 's/-.*//'`
+  VERSION=sdk-`dpkg-parsechangelog -S Version|sed 's/-.*//'`
 fi
 
 # clean old checkout
 git rm -rf vulkan-headers
 
 git clone $REPO vulkan-headers
-(cd vulkan-headers; git checkout sdk-$VERSION; cd ..)
+(cd vulkan-headers; git checkout $VERSION; cd ..)
 rm -rf vulkan-headers/.git
 
 git add -f vulkan-headers
